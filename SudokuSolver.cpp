1b43302e9317f238b797ecf1384089dee9f06a4b
Umer Amjad <m.amjad.umer@gmail.com>
2018-05-04 21:34:01 -0400

    Removed commented out debug code

--- SudokuSolver.cpp
+++ SudokuSolver.cpp
@@ -8,24 +8,19 @@ std::pair<Sudoku, bool> Solver::solve(Sudoku& sud){
     if (sud.isComplete()){
         return {sud, true};
     }
+    
     while(!sud.isDivergent()){
-        //std::cout << "Possibles filled " << std::endl; //debug
         sud = sud.fillPossibles();
         if (sud.isComplete()){
             return {sud, true};
         }
     }
-//    std::cout << sud.EmptyPositionsPossibilities();
-//    if(sud.invalidPuzzle()){
-//        return {sud, false};
-//    }
     
     auto nbrs = sud.neighbours();
     return solveSudokuNeighbours(nbrs);
 }
 
 std::pair<Sudoku, bool> Solver::solveSudokuNeighbours(std::deque<Sudoku>& nbrs){
-    //std::cout << "solveSudokuNeighbours called" << std::endl; //debug
     if (nbrs.size() == 0){
         return {Sudoku(), false};
     }
@@ -39,30 +34,23 @@ std::pair<Sudoku, bool> Solver::solveSudokuNeighbours(std::deque<Sudoku>& nbrs){
 
 void Solver::exhaustiveSolve(Sudoku& sud, std::vector<Sudoku>& solutions){
     if (sud.isComplete()){
-        //std::cout << "A. Solution found! \n" << sud << std::endl;
         solutions.emplace_back(sud);
         return;
     }
+    
     while(!sud.isDivergent()){
-        //std::cout << "Possibles filled " << std::endl; //debug
         sud = sud.fillPossibles();
         if (sud.isComplete()){
-            //std::cout << "B. Solution found! \n" << sud << std::endl;
             solutions.emplace_back(sud);
             return;
         }
     }
-    //    std::cout << sud.EmptyPositionsPossibilities();
-    //    if(sud.invalidPuzzle()){
-    //        return {sud, false};
-    //    }
     
     auto nbrs = sud.neighbours();
     exhaustiveSolveSudokuNeighbours(nbrs, solutions);
 }
 
 void Solver::exhaustiveSolveSudokuNeighbours(std::deque<Sudoku>& nbrs, std::vector<Sudoku>& solutions){
-    //std::cout << "exhaustiveSolveSudokuNeighbours called" << std::endl; //debug
     if (nbrs.size() == 0){
         return;
     }
